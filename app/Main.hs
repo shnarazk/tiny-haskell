026c8c93c4d19e3bbd683ee1f54deedd98fee066
shnarazk <narazakilaboratory@gmail.com>
2018-05-02 17:30:50 +0900

    app/Main.hs: pretty print

--- app/Main.hs
+++ app/Main.hs
@@ -8,5 +8,7 @@ main = do
   str <- getContents
   case runHaskell str of
     Left err  -> putStrLn err
-    Right exp -> do print exp
-                    print $ runInfer exp
+    Right exp -> case runInfer exp of
+                      Right (t, Just e) -> putStrLn $ show exp ++ " :: " ++ show t ++ " -- " ++ show e
+                      Right (t, Nothing) -> putStrLn $ show exp ++ " :: " ++ show t ++ " -- empty env"
+                      Left e -> putStrLn $ show exp ++ " => Error: " ++ show e
