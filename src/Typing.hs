0db7098821e1735442a650eb39822bea5e15773a
shnarazk <narazakilaboratory@gmail.com>
2018-05-04 13:52:35 +0900

    simplify 2/2

--- src/Typing.hs
+++ src/Typing.hs
@@ -149,85 +149,57 @@ type InferResult = Either TypeError (Type, TypeEnv)
 type Infer a = StateT Int (ExceptT TypeError Identity) a
 
 runInfer :: Expr -> InferResult
-runInfer e = runIdentity $ runExceptT (evalStateT (infer emptyEnv e) 0)
+runInfer e = runIdentity $ runExceptT (evalStateT (infer e emptyEnv) 0)
 
 newTypeVar :: Infer TVar
-newTypeVar = do i <- (1 +) <$> get
-                put i
-                return $ TV i
+newTypeVar = do { i <- (1 +) <$> get; put i; return $ TV i }
 
 -- | 型環境e下における与えられた式の型を返す
 -- | 型環境は部分式の評価において更新されるため、更新された型環境も返す必要がある。
-infer :: TypeEnv -> Expr -> Infer (Type, TypeEnv)
-infer e (Lit (LInt _))    = return (TInt, e)
-infer e (Lit (LBool _))   = return (TBool, e)
-infer e (Lit (LString _)) = return (TString, e)
-infer e (Ref v@(Var n)) = case find ((v ==) . fst) . unEnv =<< e of
-                            Just (_, TScheme _ t) -> return (t, e)
-                            Nothing -> do t <- newTypeVar  -- t :: TVar
-                                          let s = TScheme [t] (TVar t)
-                                          return (TVar t, extend e (Var n, s))
-infer e (Pair ls) = do
+infer :: Expr -> TypeEnv -> Infer (Type, TypeEnv)
+infer (Lit (LInt _))    e = return (TInt, e)
+infer (Lit (LBool _))   e = return (TBool, e)
+infer (Lit (LString _)) e = return (TString, e)
+infer (Ref v@(Var n))   e = case find ((v ==) . fst) . unEnv =<< e of
+                              Just (_, TScheme _ t) -> return (t, e)
+                              Nothing -> do t <- newTypeVar  -- t :: TVar
+                                            let s = TScheme [t] (TVar t)
+                                            return (TVar t, extend e (Var n, s))
+infer (Pair ls) e = do
   let loop [] ts e' = return (TTpl (reverse ts), e')
-      loop (x:xs) ts e0 = do
-        (t, e1) <- infer e0 x
-        loop xs (t:ts) e1
+      loop (x:xs) ts e0 = do { (t, e1) <- infer x e0; loop xs (t:ts) e1 }
   loop ls [] e
-infer e xp@(List ls) = do
-  let loop [] t e' = return (TLst t, e')
-      loop (x:xs) t e0 = do
-        (t', e1) <- infer e0 x
-        case unifier t t' of
-          Just u -> do
-            let e2 = subst e1 u
-            (t1, _) <- infer e2 x
-            loop xs t1 e2
-          Nothing -> throwError (UnificationFail xp t t')
-  v <- newTypeVar
-  loop ls (TVar v) e
-infer e0 (App xs) = do
+infer xp@(List ls) e = do
+  let loop [] e' t = return (TLst t, e')
+      loop (x:xs) e0 t = do { (t1, e2) <- infer x =<< uncurry (unify xp t) =<< infer x e0; loop xs e2 t1 }
+  loop ls e . TVar =<< newTypeVar
+infer (App xs) e0 = do
   let loop []    e ts = return (reverse ts, e)
-      loop (a:l) e ts = do (t', e') <- infer e a
-                           loop l e' (t' : ts)
+      loop (a:l) e ts = do { (t', e') <- infer a e; loop l e' (t' : ts) }
   (ts, e1) <- loop xs e0 []
   r2 <- TArr . (\v -> tail ts ++ [v]) . TVar <$> newTypeVar
-  let r1 = head ts
-  case unifier r1 r2 of
-    Just u  -> do let e2 = subst e1 u
-                  (ret, e3) <- infer e2 (head xs)
-                  case ret of
-                    TArr l -> return (last l, e3)
-                    _      -> return (ret, e3)
-    Nothing -> throwError (UnificationFail (head xs) r1 r2)
-infer e (Paren x) = infer e x
-infer e0 xp@(Op op x y)
-  | elem op [Add, Sub, Mul]  = do
-      (tx, e1) <- infer e0 x
-      case unifier tx TInt of
-        Just u -> do let e2 = subst e1 u
-                     (ty, e3) <- infer e2 y
-                     case unifier ty TInt of
-                       Just w -> return (TInt, subst e3 w)
-                       Nothing -> throwError (UnificationFail y ty TInt)
-        Nothing -> throwError (UnificationFail x tx TInt)
-  | elem op [Eql] = do
-      (tx, e1) <- infer e0 x
-      (ty, e2) <- infer e1 y
-      case unifier tx ty of
-        Just u -> return (TBool, subst e2 u)
-        Nothing -> throwError (UnificationFail xp tx ty)
-  | otherwise = throwError (UnificationFail xp TUnit TUnit)
-infer e0 (Let v x1 x2) = do
-  (t1, e1) <- infer e0 x1                    -- x1の型からvはt1型である（自由変数が消えるようなunifyは不可）
-  (_ , e2) <- infer e1 x2                    -- 最初から自由変数がなければ消えたりはしない。
+  (TArr l, e2) <- infer (head xs) =<< unify (head xs) (head ts) r2 e1
+  return (last l, e2)
+infer (Paren x) e = infer x e
+infer xp@(Op op x y) e0
+  | elem op [Add, Sub, Mul]  = do (tx, e1) <- infer x e0
+                                  (ty, e2) <- infer y =<< unify x tx TInt e1
+                                  (TInt,) <$> unify y ty TInt e2
+  | elem op [Eql] = do (tx, e1) <- infer x e0
+                       (ty, e2) <- infer y e1
+                       (TBool,) <$> unify xp tx ty e2
+  | otherwise = throwError $ NotImplemented xp
+infer (Let v x1 x2) e0 = do
+  (t1, e1) <- infer x1 e0                    -- x1の型からvはt1型である（自由変数が消えるようなunifyは不可）
+  (_ , e2) <- infer x2 e1                    -- 最初から自由変数がなければ消えたりはしない。
   let (Just (TScheme _ tv)) = schemeOf e2 v  -- x2での型推論よりvの型はtvでなければならない
   let (Just overlap) = not . null . intersect (tVarsIn t1) . freevars <$> e2   -- x1より束縛変数を求める
-  case (not overlap, unifier t1 tv) of
-    (True, Just u) -> do (tl, e3) <- infer (subst e2 u) x2
-                         return (tl, within (filter ((v /=) . fst)) <$> e3)
-    _              -> throwError $ UnificationFail (Ref v) t1 tv
-
-infer _ x = throwError $ NotImplemented x
+  if overlap
+    then throwError $ UnificationFail (Ref v) t1 tv
+    else do e3 <- unify (Ref v) t1 tv e2
+            (tl, e4) <- infer x2 e3
+            return (tl, within (filter ((v /=) . fst)) <$> e4)
+infer x _ = throwError $ NotImplemented x
 
 tVarsIn :: Type -> [TVar]
 tVarsIn (TCon _) = []
@@ -256,3 +228,9 @@ unifier (TLst t1) (TLst t2) = unifier t1 t2
 unifier (TTpl l1) (TTpl l2) = mconcat $ zipWith unifier l1 l2
 unifier (TArr l1) (TArr l2) = mconcat $ zipWith unifier l1 l2
 unifier _ _ = Nothing
+
+unify :: Expr -> Type -> Type -> TypeEnv -> Infer TypeEnv
+unify x t t' e =
+  case unifier t t' of
+    Just u -> return $ subst e u
+    Nothing -> throwError (UnificationFail x t t')
