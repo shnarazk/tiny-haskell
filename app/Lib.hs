9b637cb6c2bacd67d01dd7d430b4f23522a71693
shnarazk <narazakilaboratory@gmail.com>
2018-05-12 03:55:40 +0900

    use Haskell comment style for the header for AST

--- app/Lib.hs
+++ app/Lib.hs
@@ -16,5 +16,5 @@ typing str =
   case parseHaskell str of
     Left err  -> err
     Right exp -> case inferExpr exp of
-                   Right (t, e)  -> str ++ " :: " ++ prettyPrint t ++ "\t -- in " ++ prettyPrint (shadow haskellEnv e)
-                   Left e -> str ++ " => Error: " ++ prettyPrint e ++ "\n;;; The abstruct syntax tree of `" ++ str ++ "`\n" ++ show exp
+                   Right (t, e)  -> str ++ " :: " ++ prettyPrint t ++ "\t -- in " ++ prettyPrint (shadow haskellEnv e) ++ "\n-- abstruct syntax tree of `" ++ str ++ "`\n" ++ show exp
+                   Left e -> str ++ " => Error: " ++ prettyPrint e ++ "\n-- The abstruct syntax tree of `" ++ str ++ "`\n" ++ show exp
--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,15 @@
 module Main where
 import Lib
+import Control.Monad
+import System.IO
+import System.Exit
 
 main :: IO ()
-main = mapM_ (putStrLn . typing) . lines =<< getContents
+main = do
+  let prompt = putStr "TinyHaskell> " >> hFlush stdout
+      eval k
+        | k == ":q" = die "Bye."
+        | otherwise = putStrLn $ typing k
+  prompt
+  mapM_ (const prompt <=< eval) . lines =<< getContents
+
